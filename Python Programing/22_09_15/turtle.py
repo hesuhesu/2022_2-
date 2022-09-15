965502f51a16a37993594bb221acf3a8fce16218
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-09-15 12:23:19 +0900

    Create turtle.py

---
+++ Python Programing/22_09_15/turtle.py
@@ -0,0 +1,16 @@
+import turtle
+import time
+
+
+t = turtle.Turtle()
+t.shape("turtle")
+n = int(input("몇각형을 그리시겠어요?(3~6): "))
+
+for i in range(n) :
+    t.forward(100)
+    t.left(360//n)
+    
+time.sleep(10)
+
+
+turtle.done()
