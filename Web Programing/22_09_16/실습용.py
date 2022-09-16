020466b0df5acf0f3bb7c281d43f8d845c685c93
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-09-16 10:30:33 +0900

    Create 실습용.py

---
+++ Web Programing/22_09_16/실습용.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title>Welcome</title>
+</head>
+
+<body style = "background-color : lime">
+<h2>은희수의 p1 project입니다.</h2>
+<a href = "/menu">시작</a>
+</body>
+</html>
