0c22a146f64d76ec3e5dc9f63b148d207d47102a
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-10-06 13:13:18 +0900

    Add files via upload

---
+++ Python Programing/22_10_06/n각형_turtle(solo).py
@@ -0,0 +1,15 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+def n_polygon(n, length) :
+    for i in range(n):
+        t.forward(length)
+        t.left(360/n)
+
+for i in range(10) :
+    t.left(20)
+    n_polygon(6,100)
+
+turtle.done()
