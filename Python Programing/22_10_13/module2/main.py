9bedcc96dcbf0b1f3992eff9e3811983f0bb7bbf
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-10-13 13:26:53 +0900

    Add files via upload

--- Python Programing/22_10_13/module2/main.py
+++ Python Programing/22_10_13/module2/main.py
@@ -1,11 +1,10 @@
 import square2, person
 
 def main() :
-    print(square2.base)
-    print(square2.square(10))
-
-    maria = person.Person('마리아', 20, '서울시 서초구 반포동')
-    maria.greeting()
+    eun = person.Person("eun", 24, "Busan-Namgu")
+    eun.greeting()
+    square2.base, n = map(int, input("n의 m승이다. n^m을 기입하라 : ").split("^"))
+    print(square2.square(n))
 
 if __name__ == "__main__" :
     main()
---
+++ Python Programing/22_10_13/module2/square2.py
@@ -0,0 +1,7 @@
+base = 2
+
+def square(n) :
+    return base **n
+
+if __name__ == '__main__' :
+    print(square(3))
