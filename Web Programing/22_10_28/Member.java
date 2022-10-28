f18cf225edccb46650966b2ee5bd2831e03b94b7
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-10-28 13:45:39 +0900

    Add files via upload

---
+++ Web Programing/22_10_28/Member.java
@@ -0,0 +1,16 @@
+package com.webp.p5;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Member {
+	@Id public String id;
+	public String pw;
+	public String name;
+	public String phone;
+	public Integer balance;
+	@CreationTimestamp public LocalDateTime rdate;
+}
