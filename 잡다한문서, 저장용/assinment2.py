ab89bda2a805c8c2902390463d061a1e4935981e
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-02 01:40:28 +0900

    Update assinment2.py

--- 잡다한문서, 저장용/assinment2.py
+++ 잡다한문서, 저장용/assinment2.py
@@ -7,17 +7,19 @@ a2 = '{}\\replacement_output.txt'.format(a)
 Test1 = open(a1, 'r', encoding = 'utf8')
 Test2 = open(a2, 'w', encoding = 'utf8')
 
-listnum = 0
-
 ab = int(Test1.readline())         # 계산을 반복할 횟수를 불러온다.
 
+listnum = 0
+Anum = 1
 for i in range(ab*2) :
     
     if i % 2 == 0 :
         bc = int(Test1.readline())
         print("\n----------------------------------------------")
-        print("\n아래 리스트의 갯수는 {}입니다.\n".format(bc))
+        print("\n{}번째 리스트의 갯수는 {}입니다.\n".format(Anum,bc))
+        Anum += 1
     else :
+        
         listA = Test1.readline()
         inputs = listA.split()
         print("----------------------------------------------\n")
@@ -26,8 +28,7 @@ for i in range(ab*2) :
         buffer = []         # 체킹 버퍼
         File_content = []
         outputLine1 = []        # run의 개수
-        outputLine2 = []        # 임시로 저장하는 곳
-        outputLine3 = []
+        outputLine2 = []        # 임시로 run 리스트들 저장하는 곳
         line = 1               # 줄 갯수를 의미.
         buffer_start_number = 0 # 동결되고 난 후의 시작번호
 
@@ -35,35 +36,115 @@ for i in range(ab*2) :
         
         buffer = sorted(inputs[:5])
         del inputs[:5]
-        print("{}는 제일 작은 값입니다.\n".format(buffer[0]))
-        File_content.append(buffer[0]) # run에 계속 추가
-        print("버퍼의 현재 상황 : {}".format(buffer))
+        File_content.append(buffer[0])
+        print("최최의 버퍼의 상황 : {}".format(buffer))
+        del buffer[buffer_start_number]
+        print("최초의 잠시 파일 콘텐츠 현재 상황 : {}".format(File_content))
+        
+        listnum = 0
+        anynum = 0
 
         while 1 : 
 
-            buffer.append(inputs[0])
-            buffer.sort()
-            del inputs[0]
-            print("{}는 제일 작은 값입니다.\n".format(buffer[0]))
+            if listnum == 0 :
+                listnum += 1
+                
+                pass
+            if File_content == None :
+                File_content.append(-100)
 
-            if buffer[buffer_start_number] >= File_content[-1] :
-                File_content.append(buffer[buffer_start_number])
-                del buffer[buffer_start_number]
-                print("나간 뒤 버퍼의 현재 상황(4칸) : {}".format(buffer))
             
+
+            try :
+                buffer.append(inputs[0])
+                buffer.sort()
+                del inputs[0]
+                
+            except :
+                buffer.sort()
+                
+
+            try : 
+                if buffer[buffer_start_number] >= File_content[-1] :
+                    File_content.append(buffer[buffer_start_number])
+                    print("버퍼의 상황 : {}".format(buffer))
+                    del buffer[buffer_start_number]
+                    print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                
+                elif buffer[buffer_start_number] < File_content[-1] :
+                    buffer_start_number += 1
+                    if buffer_start_number < 5 :
+                        File_content.append(buffer[buffer_start_number])
+                        print("버퍼의 상황 : {}".format(buffer))
+                        del buffer[buffer_start_number]
+                        print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                    
+                    else :
+                        line += 1
+                        outputLine2.append(File_content)
+                        File_content = []
+                        buffer_start_number = 0
+            except : 
+                try :
+                    if buffer[-1] >= File_content[-1] :
+                        File_content.append(buffer[0])
+                        print("버퍼의 상황 : {}".format(buffer))
+                        del buffer[0]
+                        print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                
+                    elif buffer[-1] < File_content[-1] :
+                        buffer_start_number += 1
+                        if buffer_start_number < 5 :
+                            File_content.append(buffer[0])
+                            print("버퍼의 상황 : {}".format(buffer))
+                            del buffer[0]
+                            print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                    
+                        else :
+                            line += 1
+                            outputLine2.append(File_content)
+                            File_content = []
+                            buffer_start_number = 0
+                except : 
+                    break
+
+                        
+                    
+            
+
+            
+            '''
+            if (buffer[buffer_start_number] >= File_content[-1]):
+                File_content.append(buffer[buffer_start_number])
+                print("버퍼의 상황 : {}".format(buffer))
+                del buffer[buffer_start_number]
+                print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                
             elif buffer[buffer_start_number] < File_content[-1] :
                 buffer_start_number += 1
                 if buffer_start_number < 5 :
                     File_content.append(buffer[buffer_start_number])
+                    print("버퍼의 상황 : {}".format(buffer))
                     del buffer[buffer_start_number]
-                    print("나간 뒤 버퍼의 현재 상황(4칸) : {}".format(buffer))
+                    print("파일 콘텐츠의 현재 상황 : {}".format(File_content))
+                    
                 else :
                     line += 1
-                    
-        
+                    outputLine2.append(File_content)
+                    File_content = []
+                    buffer_start_number = 0
+            '''
+    
+    '''
+    Test2.write(line)                         # 여기를 풀면 답이 나올듯
+    
+    for i in range(len(outputLine2)) :
 
-        Test2.write(line)
-        Test2.write()
+        for j in range(len(outputLine2[i])) :
+            Test2.write(outputLine2[i][j])
+    '''
+                
+        
                 
                 
             
