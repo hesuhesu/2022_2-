214937f21ac3a13528a59fc745c0368dc803b3db
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-04 16:29:30 +0900

    Add files via upload

---
+++ Java Programing/22_11_04/CircleTest2.java
@@ -0,0 +1,24 @@
+package practice;
+
+class Circle {
+	int radius;
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	public boolean equals(Circle c1) {
+		if(radius == c1.radius) return true;
+		else return false;
+	}
+}
+
+public class CircleTest2 {
+
+	public static void main(String[] args) {
+		Circle obj1 = new Circle(10);
+		Circle obj2 = new Circle(10);
+		if(obj1.equals(obj2)) System.out.println("2개의 원은 같습니다.");
+		else System.out.println("2개의 원은 같지 않습니다.");
+
+	}
+
+}
