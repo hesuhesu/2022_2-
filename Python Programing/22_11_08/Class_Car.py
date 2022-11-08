8fb4c6789dc54fdfa86ea3ac7fe85a9d2ae4f957
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-08 11:34:56 +0900

    Update Class_Car.py

--- Python Programing/22_11_08/Class_Car.py
+++ Python Programing/22_11_08/Class_Car.py
@@ -20,9 +20,17 @@ myCar2 = Car("파랑", 60)
 print(myCar1.speed, myCar2.speed)
 
 while 1 :
+    
+    print("--------------------------------------------")
     n1 = int(input("myCar1가 가속할 속도를 입력하시오 : "))
-    if n1 == 0 :
+    if n1 == -1 :
         break
     myCar1.upSpeed(n1)
+    
+    n2 = int(input("myCar1가 감속할 속도를 입력하시오 : "))
+    if n2 == -1 :
+        break
+    myCar1.downSpeed(n2)
+    print("--------------------------------------------")
 
 print("최종 속력은 {}".format(myCar1.speed))
