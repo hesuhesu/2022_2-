e2e268237082bd4fb3bb0dd4c3b6882dac80a9a3
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-09 17:17:58 +0900

    Create FrameTest.java

---
+++ Java Programing/22_11_09/FrameTest.java
@@ -0,0 +1,16 @@
+package practice1;
+
+import javax.swing.*;
+
+public class FrameTest {
+
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame("Frame Test");
+		f.setTitle("MyFrame");
+		f.setSize(300,200);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
