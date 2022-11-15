45626edec6b0ae7d99d23254fb285b2791e15203
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-15 10:14:49 +0900

    Update login.java

--- Java Programing/Report/login.java
+++ Java Programing/Report/login.java
@@ -3,7 +3,6 @@ package practice1;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,14 +13,21 @@ import javax.swing.JTextField;
 public class login extends JFrame{
 	public login() {
 		
+		JTextField field1 = new JTextField(10);
+		JTextField field2 = new JTextField(10);
+		
 		class MyListener implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "You failed to log in");
+				
+				if (field1.getText().equals("test1") && field2.getText().equals("1234")) { // 단순하게 하나만 로그인 할 수 있는 id, pw를 생성했습니다.
+					JOptionPane.showMessageDialog(null, "Welcome");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "You failed to log in");
+				}
+				
 			}
 		}
-		
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("manson23", "1234");
 		
 		
 		setSize(800,500);
@@ -30,9 +36,9 @@ public class login extends JFrame{
 		setLayout(new FlowLayout());
 		
 		JLabel label1 = new JLabel("ID : ");
-		JTextField field1 = new JTextField(10);
+		
 		JLabel label2 = new JLabel("PassWord : ");
-		JTextField field2 = new JTextField(10);
+		
 		JButton button = new JButton("Login");
 		button.addActionListener(new MyListener());
 		
