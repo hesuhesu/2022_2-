ada2eb9f5f40a121a116333ff2c2ae7cc3c7c37b
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2022-11-25 16:04:19 +0900

    Add files via upload

---
+++ Java Programing/22_11_25/BouncingBall.java
@@ -0,0 +1,55 @@
+package practice;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class BouncingBall extends JFrame implements ActionListener {
+	static final int WIDTH = 1200;
+	static final int HEIGHT = 600;
+	private static final int PERIOD = 10;
+
+	class MyPanel extends JPanel {
+		int x=0, y=0, xInc=3, yInc=3, diameter=60;
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+				if (x < 0 || x > (BouncingBall.WIDTH - diameter))
+					xInc = -xInc;
+				if (y < 0 || y > (BouncingBall.HEIGHT - diameter))
+					yInc = -yInc;
+
+				x += xInc;
+				y += yInc;
+				g.setColor(Color.RED);
+				g.fillOval(x, y, diameter, diameter);
+		}
+	}
+	public BouncingBall() {
+		MyPanel panel = new MyPanel();
+
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		add(panel);
+		pack();
+		setTitle("Bouncing Ball");
+		Timer timer = new Timer(PERIOD, this);
+		timer.start();
+
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		repaint();
+	}
+	public static void main(String[] args) {
+		BouncingBall f = new BouncingBall();
+	}
+
+}
