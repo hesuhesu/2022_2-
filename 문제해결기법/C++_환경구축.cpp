a31f5bded24d422a44e78d9113ddcd87ebcf01e1
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2023-03-13 14:36:11 +0900

    Create C++_환경구축.cpp

---
+++ 문제해결기법/C++_환경구축.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int b, c;
+    cout << "Hello world!" << "\n"; // faster than endl
+    cout << "안녕 세상아" << endl;
+
+    scanf("%d", &b); // faster than cin
+    cin >> c;
+    cout << b * 3 << endl;
+    printf("%d", c * 3)
+    return 0;
+}
