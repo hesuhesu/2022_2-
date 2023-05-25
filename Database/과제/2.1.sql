80063cb5a3704053c2334b68620f4ab2b45952d6
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2023-05-25 12:06:02 +0900

    Add files via upload

---
+++ Database/과제/2.1.sql
@@ -0,0 +1,6 @@
+insert into
+Book(bookid, bookname, publisher, price)
+VALUES(11,  '수학의 정석', '대한수학협회', 60000);
+
+select *
+from Book;
---
+++ Database/과제/2.3.sql
@@ -0,0 +1,5 @@
+delete from Book;
+
+show tables;
+select *
+from Book;
