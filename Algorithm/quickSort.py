c008694cc82198de80fccf03e08ecbe2a4f8ecf4
hesuhesu <zndwk0728@gmail.com>
2023-09-22 20:51:39 +0900

    first assignment : quickSort update

---
+++ Algorithm/quickSort.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def quick_sort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while arr[low] < pivot:
+                low += 1
+            while arr[high] > pivot:
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+
+    return sort(0, len(arr) - 1)
+
+excel_source = pd.read_excel('input_quick_sort.xlsx')
+listA = []
+for i in range(100) :
+    listA.append(excel_source.head(i))
+
+quick_sort(listA)
+print("정렬 결과 : ", listA)
