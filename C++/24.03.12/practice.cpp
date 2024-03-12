33c20cc54257cd5fa4c17a1cb474d9978e25122a
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-03-12 12:55:00 +0900

    Create practice.cpp

---
+++ C++/24.03.12/practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string n, str;
+	cout << "input student num : ";
+	cin >> n;
+	cout << "input student name : ";
+	cin >> str;
+	cout << "student num : " << n << ", student name : " << str << "\n";
+	return 0;
+
+}
