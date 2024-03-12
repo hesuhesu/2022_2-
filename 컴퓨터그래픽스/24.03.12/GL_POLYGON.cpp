7d6015374960d7e8b6bd4cb556fbd788cd092a50
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-03-12 14:06:42 +0900

    Create GL_POLYGON.cpp

---
+++ 컴퓨터그래픽스/24.03.12/GL_POLYGON.cpp
@@ -0,0 +1,28 @@
+#include "gl/glut.h"
+
+void display() {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.0f, 0.0f, 0.0f); // 0.0 ~ 1.0
+    // glPointSize(10.0);
+
+    glBegin(GL_POLYGON); // GL_POLYGON, GL_TRIANGLES, GL_QUADS, GL_LINES, GL_POINTS
+    glVertex2f(-0.5f, -0.5f); //도형에 대한 정점(꼭지점)
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
+    glEnd();
+
+    glFinish();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL");
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
