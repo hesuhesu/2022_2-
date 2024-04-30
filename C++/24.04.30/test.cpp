447932fd6fbe84baedea8de97fe401b2a3a6d7cb
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-04-30 18:52:28 +0900

    Update test.cpp

--- C++/24.04.30/test.cpp
+++ C++/24.04.30/test.cpp
@@ -15,7 +15,7 @@ public:
 
 class Family {
 	Person* p;
-	string familyName;
+	string familyName; 
 	int size;
 public:
 	Family(string name, int size) {
@@ -24,15 +24,15 @@ public:
 		this->familyName = name;
 	}
 	void show() {
-		cout << familyName << "의 가족은 다음과 같습니다.\n";
+		cout << familyName << "가족은 다음과 같이 " << size << "명 입니다.\n";
 		for (int i = 0; i < size; i++) {
-			cout << (p + i)->getName() << " ";
+			cout << (p+i)->getName() << "	 ";
 		}
 	}
 	void setName(int index, string name) {
-		(p + index)->setName(name);
+		(p+index)->setName(name);
 	}
-	~Family();
+	~Family() { delete[]p; }
 };
 
 int main() {
