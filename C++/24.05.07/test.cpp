cec5cfe500dfdf86feb82f9b260cb822dd82fce8
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-05-07 20:06:34 +0900

    Update test.cpp

--- C++/24.05.07/test.cpp
+++ C++/24.05.07/test.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 bool bigger(int a, int b, int& big) {
 	if (a == b) true;
 	else {
-		if (a > b) {
-			big = a;
-		}
-		else {
-			big = b;
-		}
+		if (a > b) big = a;
+		else big = b;
 		return false;
 	}
 }
@@ -18,7 +15,7 @@ int main() {
 	int x, y;
 	cin >> x >> y;
 	int big;
-	
+
 	if (bigger(x, y, big)) cout << "값이 같음";
-	else cout << "값이 다름 -> " << big;
+	else cout << "값이 다름.. 큰 값은 " << big;
 }
