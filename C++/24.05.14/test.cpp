b8a59285e212490253fc56ba12210812c9479c69
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-05-14 13:15:43 +0900

    Create test.cpp

---
+++ C++/24.05.14/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Person {
+	int id;
+	double weigth;
+	string name;
+public:
+	Person() {
+		id = 1;
+		name = "Grace";
+		weigth = 20.5;
+	}
+	/*Person(int id = 1, string name = "Grace", double weigth = 20.5) {
+		this->id = id;
+		this->name = name;
+		this->weigth = weigth;
+	}*/
+	Person(int id, string name, double weigth = 20.5) {
+		this->id = id;
+		this->name = name;
+		this->weigth = weigth;
+	}
+	void show() { cout << id << " " << weigth << " " << name << "\n"; }
+};
+
+int main() {
+
+	Person grace, ashley(2, "Ashley"), hellen(3, "Hellen", 32.5);
+	grace.show();
+	ashley.show();
+	hellen.show();
+	return 0;
+
+}
