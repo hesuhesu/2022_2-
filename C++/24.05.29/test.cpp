0a029ff301235bce66829ee76a2ef374d5ae86e7
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-05-29 21:00:52 +0900

    Update test.cpp

--- C++/24.05.29/test.cpp
+++ C++/24.05.29/test.cpp
@@ -7,27 +7,26 @@ class Circle {
 public:
 	Circle(int radius = 0) { this->radius = radius; }
 	void show() { cout << "radius = " << radius << "인 원\n"; }
-	friend Circle& operator++(Circle& op); // 전위 ++ 연산자 함수 프렌드 선언
-	friend Circle operator++(Circle& op, int x); // 후위 ++ 연산자 함수 프렌드 선언
-	friend Circle operator+(int op1, Circle op2); // 프렌드 선언
-
+	friend Circle& operator++(Circle& op);
+	friend Circle operator++(Circle& op, int x);
+	friend Circle operator+(int op1, Circle op2);
 };
 
-Circle& operator++(Circle& op) { // 전위 ++ 연산자 함수 구현
+Circle& operator++(Circle& op) { 
 	op.radius++;
-	return op; // 연산 결과 리턴
+	return op;
 }
 
-Circle operator++(Circle& op, int x) { // 후위 ++ 연산자 함수 구현
-	Circle tmp = op; // 변경하기 전의 op 상태 저장
+Circle operator++(Circle& op, int x) { 
+	Circle tmp = op; 
 	op.radius++;
-	return tmp; // 변경 이전의 op 리턴
+	return tmp;
 }
 
 Circle operator+(int op1, Circle op2) {
-	Circle tmp; // 임시 객체 생성
+	Circle tmp;
 	tmp.radius = op1 + op2.radius;
-	return tmp; // 임시 객체 리턴
+	return tmp; 
 }
 
 int main() {
