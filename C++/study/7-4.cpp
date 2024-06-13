61a469ee5b004d4f67b7d14956e7121a58084412
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-06-14 00:09:38 +0900

    Create 7-4.cpp

---
+++ C++/study/7-4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick; this->punch = punch;
+	}
+	void show();
+	Power operator+ (Power op2); // + 연산자 함수 선언
+};
+
+void Power::show() {
+	cout << "kick=" << kick << ',' << "punch=" << punch << endl;
+}
+
+Power Power::operator+(Power op2) {
+	Power tmp; // 임시 객체 생성
+	tmp.kick = this->kick + op2.kick; // kick 더하기
+	tmp.punch = this->punch + op2.punch; // punch 더하기
+	return tmp; // 더한 결과 리턴
+}
+
+int main() {
+	Power a(3, 5), b(4, 6), c;
+	c = a + b; // 파워 객체 + 연산
+	a.show();
+	b.show();
+	c.show();
+
+	return 0;
+
+	/*
+	kick=3,punch=5
+	kick=4,punch=6
+	kick=7,punch=11
+	*/
+}
