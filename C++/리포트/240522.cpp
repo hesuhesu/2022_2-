dae431a4662c89453fc041eeb1cd51ae449044f9
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-06-19 15:40:16 +0900

    Update 240522.cpp

--- C++/리포트/240522.cpp
+++ C++/리포트/240522.cpp
@@ -2,30 +2,36 @@
 
 using namespace std;
 
-class Board {
+class Book {
+	string title;
+	int price, pages;
 public:
-	static string* notices;
-	static int index;
-	static void add(string str) { notices[index++] = str; }
-	static void print() {
-		cout << "************** 게시판입니다. **************" << endl;
-		for (int i = 0; i < index; i++) {
-			cout << i << " : " << notices[i] << "\n";
-		}
+	Book(string title = "", int price = 0, int pages = 0) {
+		this->title = title; this->price = price; this->pages = pages;
+	}
+	void show() {
+		cout << title << " " << price << "원 " << pages << " 페이지" << endl;
+	}
+	string getTitle() { return title; }
+	bool operator ==(int price) {
+		if (this->price == price) return true;
+		else return false;
+	}
+	bool operator ==(string title) {
+		if (this->title == title) return true;
+		else return false;
+	}
+	bool operator ==(Book& b) {
+		if (this->title == b.title && this->price == b.price && this->pages == b.pages) return true;
+		else return false;
 	}
 };
 
-string* Board::notices = new string[100];
-int Board::index = 0;
-
 int main() {
 
-	Board::add("중간고사는 감독 없는 자율 시험입니다.");
-	Board::add("코딩 라운지 많이 이용해 주세요");
-	Board::print();
-	Board::add("리포트 대마왕 학생이 경진대회 입상하였습니다. 축하해주세요");
-	Board::print();
-	Board::add("매일 매일 코딩 공부는 꾸준히 해야 합니다.");
-	Board::print();
-	return 0;
+	Book a("명품 C++", 30000, 500), b("고품 c++", 30000, 500);
+	if (a == 30000) cout << "정가 30000원\n";
+	if (a == "명품 C++") cout << "명품 C++ 입니다.\n";
+	if (a == b) cout << "두 책이 같은 책입니다.\n";
+
 }
